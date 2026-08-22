2ab1545f3c3edd1951a87a6c148fd34f417bb8d0
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-22 19:10:13 -0300

    fix: Correção do nome da função parser para parse

---
+++ src/utils/parse.js
@@ -0,0 +1,24 @@
+import { normalizarTexto } from "./normalizarTexto.js"
+const regex = /^(.+)\s+(\d+)$/
+export function parse(texto) {
+    const dados = texto.split("\n").map((m) => {
+        const formatacaoM = m.trim()
+        const match = formatacaoM.replace(/\s+/g, " ").match(regex)
+        if (match === null) {
+            return { erro: true, mensagem: `Informação faltando perto de ${formatacaoM}, favor inserir os valores` }
+        }
+        return {
+            nome: normalizarTexto(match[1]),
+            quantidadeG: match[2]
+        }
+    })
+    const erros = dados.filter(item => item.erro === true)
+    const validos = dados.filter(item => item.erro !== true)
+
+    if (erros.length > 0) {
+        return erros
+    }
+    return validos
+}
+
+
