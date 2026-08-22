b7d394d88d05acac078040d44335df11260e233b
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-22 19:11:42 -0300

    fix: Alteração do path + nome da função e add do id

---
+++ src/services/calcularMacros.js
@@ -0,0 +1,49 @@
+import { PrismaClient } from "../../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL
+});
+
+const prisma = new PrismaClient({ adapter });
+
+
+export async function calcularMacros(itens) {
+    const nomes = itens.map(item => item.nome);
+
+    const alimentos = await prisma.alimento.findMany({
+        where: {
+            nomeBusca: {
+                in: nomes
+            }
+        }
+    });
+    const resultados = itens.map(item => {
+        const alimento = alimentos.find(a => a.nomeBusca === item.nome)
+        if (!alimento) {
+            return { erro: true, message: `Alimento ${item.nome} não encontrado` }
+        }
+        const kcal = (Number(item.quantidadeG) * alimento.kcal) / alimento.porcaoReferenciaG
+        const carboidrato = (Number(item.quantidadeG) * alimento.carboidrato) / alimento.porcaoReferenciaG
+        const proteina = (Number(item.quantidadeG) * alimento.proteina) / alimento.porcaoReferenciaG
+        const gordura = (Number(item.quantidadeG) * alimento.gordura) / alimento.porcaoReferenciaG
+        return {
+            alimentoId: alimento.id,
+            nome: item.nome,
+            quantidadeG: Number(item.quantidadeG),
+            kcal,
+            carboidrato,
+            proteina,
+            gordura
+        }
+    })
+    const erros = resultados.filter(item => item.erro === true)
+    const validos = resultados.filter(item => item.erro !== true)
+    if (erros.length > 0) {
+        return erros
+
+    }
+    return validos
+
+}
