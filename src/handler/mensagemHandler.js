60f5018ec5fe4d6d308e956a05c9e3e1e6cfcac7
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-24 17:23:21 -0300

    fix: reformulação de estrutura

--- src/handler/mensagemHandler.js
+++ src/handler/mensagemHandler.js
@@ -1,18 +1,5 @@
-import { extrairTipoRefeicao } from "../services/extrairTipoRefeicao";
-import { salvarRefeicao } from "../services/salvarRefeicao";
-import { calcularMacros } from "../services/calcularMacros";
-import { parse } from "../utils/parse";
-import { agregacaoService } from "../services/agregacaoService";
-
-
 async function mensagemHandler(mensagem) {
-    const refeicao = extrairTipoRefeicao(mensagem)
-    const alimentosTexto = refeicao.alimentos
-    const tipoRefeicao = refeicao.refeicao
-    const alimentos = parse(alimentosTexto)
-    const tabelaNutricionalAlimento = await calcularMacros(alimentos)
-    const resultadoTeste = await salvarRefeicao(tabelaNutricionalAlimento, tipoRefeicao)
-    const resultado = await agregacaoService()
+    
 }
 
-mensagemHandler("Almoço \nArroz 150\nFeijão 100")
+mensagemHandler()
