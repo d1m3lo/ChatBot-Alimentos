714b6b5f8cf51adce4e9ef3d249e5c7cc7afdde8
d1m3lo <gabrieldemello.rodrigues@gmail.com>
2026-08-25 13:35:13 -0300

    feat: arquivo responsável pela comunicação do chatBot com o backend (recebimento, tratamento e envio)

--- src/handler/mensagemHandler.js
+++ src/handler/mensagemHandler.js
@@ -1,5 +1,21 @@
-async function mensagemHandler(mensagem) {
-    
-}
+import { Bot } from "grammy"
+import { processarRefeicao } from "../services/processarRefeicao";
+import 'dotenv/config'
+const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
+bot.command("start", (ctx) => ctx.reply("Bot pronto! Me manda sua refeição no formato: tipo de refeição na primeira linha, alimentos e quantidade nas linhas seguintes."))
+bot.on("message:text", async (ctx) => {
+    const { resultado, erros } = await processarRefeicao(ctx.message.text)
+    if (erros.length > 0) {
+        const mensagemErro = erros.map((erro) => erro.message).join("\n")
+        ctx.reply(mensagemErro)
+    }
+    if (!resultado) {
+        return
+    }
+    const resultadoRef = [`📊 Consumo diário`, `🔥 Calorias`, `meta: ${resultado.kcal.meta}`, `consumido: ${resultado.kcal.consumido}`, `saldo: ${resultado.kcal.saldo}`, ``, `🍚 Carboidratos`, `meta: ${resultado.carboidrato.meta}`, `consumido: ${resultado.carboidrato.consumido}`, `saldo: ${resultado.carboidrato.saldo}`, ``, `🥩 Proteínas`, `meta: ${resultado.proteina.meta}`, `consumido: ${resultado.proteina.consumido}`, `saldo: ${resultado.proteina.saldo}`, ``, `🥑 Gorduras`, `meta: ${resultado.gordura.meta}`, `consumido: ${resultado.gordura.consumido}`, `saldo: ${resultado.gordura.saldo}`]
+    const mensagemFormatada = resultadoRef.join("\n")
+    ctx.reply(mensagemFormatada)
+})
 
-mensagemHandler()
+bot.start()
+
